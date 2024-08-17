50279518f043ba6e786e6f17948c52f8c82c6c2d
danielwlaufs <145431238+danielwlaufs@users.noreply.github.com>
2024-08-17 02:20:25 +0200

    Update login.js

--- login.js
+++ login.js
@@ -1,11 +1,3 @@
-// JavaScript to handle the login button functionality
-
-function redirectToMain() {
-    window.location.href = 'platform.html'; // Redirect to the platform page
-}
-
-// Attach event listener to the login button
 document.getElementById('login-btn').addEventListener('click', function(e) {
-    e.preventDefault(); // Prevent any default form submission behavior
-    redirectToMain(); // Directly redirect to the main page
+    window.location.href = 'platform.html'; // Directly redirect to platform.html
 });
