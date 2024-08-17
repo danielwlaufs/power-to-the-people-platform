06ddd06822c7ca6e538d7644582c7ea0192e845b
danielwlaufs <145431238+danielwlaufs@users.noreply.github.com>
2024-08-17 03:02:30 +0200

    Update login.js

--- login.js
+++ login.js
@@ -1,3 +1,5 @@
-document.getElementById('login-btn').addEventListener('click', function(e) {
-    window.location.href = 'platform.html'; // Directly redirect to platform.html
+// Add an event listener to the login buttondocument.getElementById('login-btn').addEventListener('click', function(e) {
+    // Show an alert to confirm the button is being clickedalert('Button clicked!');
+
+    // Redirect the user to the platform.html pagewindow.location.href = './platform.html';
 });
